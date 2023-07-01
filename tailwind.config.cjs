86227bf1b1eca48c030803be73faf565b7bc75e3
Afochar Syabnirusmanto <aflowchars@gmail.com>
2023-07-01 16:44:01 +0700

    update blog read

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,10 @@ module.exports = {
         arvo: ["Arvo", "sans-serif"],
         "source-sans": ["Source Sans Pro", "sans-serif"],
       },
+      backgroundColor: {
+        "gradient-1":
+          "linear-gradient(180deg, rgba(255, 255, 255, 0.00) 0%, #FFF 77.03%)",
+      },
     },
   },
   plugins: [],
